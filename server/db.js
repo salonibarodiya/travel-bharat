99811ce3048ec3e0cf4416b258521bd7ff96060a
saloni barodiya <salonibarodiya2001@gmail.com>
2026-06-12 02:51:10 +0530

    Update MongoDB connection to use local URI
    Replaced process.env.MONGO_URI with a direct local MongoDB connection string.

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        // Humne process.env.MONGO_URI ki jagah direct local link likh diya
+        
         const localURI = "mongodb://127.0.0.1:27017/travel_bharat";
         const conn = await mongoose.connect(localURI);
         console.log(`📊 Local MongoDB Connected Successfully: ${conn.connection.host}`);
