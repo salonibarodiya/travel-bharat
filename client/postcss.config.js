9ee4799602ead3af6deb8194fcfee02975f3382d
saloni barodiya <salonibarodiya2001@gmail.com>
2026-06-12 02:43:59 +0530

    Refactor comment in postcss.config.js
    Updated comment for clarity regarding Tailwind CSS.

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,6 +1,6 @@
 export default {
   plugins: {
-    '@tailwindcss/postcss': {}, // Purane 'tailwindcss' ki jagah yeh naya naam aayega
+    '@tailwindcss/postcss': {}, 
     autoprefixer: {},
   },
 }
