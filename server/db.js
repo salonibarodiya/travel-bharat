aa78dadef86571a4281e5de48c3fee1360341442
salonibarodiya <salonibarodiya2001@gmail.com>
2026-06-12 21:26:54 +0530

    fix: update db connection to use cloud URI on production

--- server/db.js
+++ server/db.js
@@ -3,9 +3,16 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         
+<<<<<<< HEAD
         const localURI = "mongodb://127.0.0.1:27017/travel_bharat";
         const conn = await mongoose.connect(localURI);
         console.log(`📊 Local MongoDB Connected Successfully: ${conn.connection.host}`);
+=======
+        const dbURI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/travel_bharat";
+        
+        const conn = await mongoose.connect(dbURI);
+        console.log(`📊 MongoDB Connected Successfully: ${conn.connection.host}`);
+>>>>>>> d778de5 (fix: update db connection to use cloud URI on production)
     } catch (error) {
         console.error(`❌ Database Connection Error: ${error.message}`);
         process.exit(1); 
